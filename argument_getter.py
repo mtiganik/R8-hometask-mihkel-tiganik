01dbdd520409e10fdcc8d0178fe00b7d3c00e70d
Mihkel Tiganik <mtiganik@gmail.com>
2023-06-15 23:16:51 +0300

    feat: unit tests for argument getter

--- argument_getter.py
+++ argument_getter.py
@@ -1,9 +1,14 @@
 import sys, getopt 
 
+
 DEFAULT_PAGE = 1
 DEFAULT_PER_PAGE = 10
 DEFAULT_COMMENTS_COUNT_DISABLED = False
 DEFAUlt_SHOW_IN_LIST = False
+
+def getDefaults():
+    return {'page':DEFAULT_PAGE, 'per_page':DEFAULT_PER_PAGE, 'no_comment':DEFAULT_COMMENTS_COUNT_DISABLED, 'list': DEFAUlt_SHOW_IN_LIST}
+    
 
 def generateHelp():
     str1 = "This is an example program to fetch data from https://jsonmock.hackerrank.com/api/articles. \nIt sorts articles by number of comments that they have. It will make list."
@@ -16,32 +21,32 @@ def generateHelp():
     str8 = "-p  --page \t Page number of data. Default is 1"
     str9 = "-t  --per_page \t Number of articles per page. Default is 10"
     arr = [str1, str2, str3, str4, str5, str6, str7, str8, str9]
-    print("Hello")
     for x in arr:
        print(x)
 
 def getInt(num):
-    try:
+    try: 
         res = int(num)
     except ValueError:
         print(f"Cannot convert {num} to integer")
+        raise ValueError
         sys.exit()
     return res
 
 
 
 def getArgs(argv):
-    opts, args = getopt.getopt(argv,"hlp:t:",["help","page=", "per_page=","no_comment", "list"])
-    res = {'page':DEFAULT_PAGE, 'per_page':DEFAULT_PER_PAGE, 'no_comment':DEFAULT_COMMENTS_COUNT_DISABLED, 'list': DEFAUlt_SHOW_IN_LIST}
+    opts, args = getopt.getopt(argv,"hlp:t:",["help","page=", "per_page=","no_comment","no_comments", "list"])
+    res = getDefaults()
     for opt, arg in opts:
         if opt in ('-h','--help'):
             generateHelp()
             sys.exit()
         elif opt in ("-l", "--list"):
             res['list'] = True
-        elif opt in ("--no_comment"):
+        elif opt in ("--no_comment", "--no_comments"):
             res['no_comment'] = True
-        elif opt in ('-p', '--per_page'):
+        elif opt in ('-p', '--page'):
             res['page'] = getInt(arg)
         elif opt in ('-t', '--per_page'):
             res['per_page'] = getInt(arg)
--- test_argument_getter.py
+++ test_argument_getter.py
@@ -1,9 +1,25 @@
 from unittest import TestCase
-from unittest.mock import patch
-import asyncio
+from unittest.mock import MagicMock, patch
+from argument_getter import getArgs, getDefaults
 
-from argument_getter import getArgs
+import getopt
 
-class TestFilterByComments(TestCase):
-
-    def testNoArgsGiveDefaultValues
+class TestArgumentGetter(TestCase):
+    
+    def testWrongArgumentRaises(self):
+        args = ['--some','random','args']
+        self.assertRaises(getopt.GetoptError, getArgs, args)
+    
+    def testInvalidPageCntRaises(self):
+        args = ['--page', 'abc']
+        self.assertRaises(ValueError, getArgs, args )
+    
+    def testListChangesResult(self):
+        args = ['--list', '-p','3','--per_page','5']
+        expected = {'page':3,'per_page': 5, 'no_comment':False,'list':True}
+        self.assertEqual(expected,getArgs(args))
+    
+    def testCommentChanges(self):
+        args = ['--page','2','-t','3','--no_comments']
+        excpected = {'page':2,'per_page':3,'no_comment':True,'list':False}
+        self.assertEqual(excpected,getArgs(args))
