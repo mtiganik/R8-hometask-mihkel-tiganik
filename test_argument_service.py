18d007faf1996221a710b05bbac5101ec2ff129a
Mihkel Tiganik <mtiganik@gmail.com>
2023-06-21 09:24:28 +0300

    fix: fixed unittest bug

--- test_argument_service.py
+++ test_argument_service.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from unittest.mock import MagicMock, patch
-from services.argument_service import getArgs, getDefaults
+from argument_service import getArgs, getDefaults
 
 import getopt
 
